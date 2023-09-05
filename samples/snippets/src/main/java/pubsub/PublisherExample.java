a196083cfd1ccd06d870353fc2fb11c73db5bebc
Kamal Aboul-Hosn <kamal.aboulhosn@gmail.com>
2023-09-05 12:03:52 -0400

    Update publish BQ example

--- samples/snippets/src/main/java/pubsub/PublisherExample.java
+++ samples/snippets/src/main/java/pubsub/PublisherExample.java
@@ -20,19 +20,27 @@ package pubsub;
 // [START pubsub_publish]
 
 import com.google.api.core.ApiFuture;
+import com.google.api.core.ApiFutureCallback;
+import com.google.api.core.ApiFutures;
+import com.google.api.gax.batching.BatchingSettings;
+import com.google.api.gax.batching.FlowControlSettings;
+import com.google.api.gax.batching.FlowController.LimitExceededBehavior;
+import com.google.api.gax.rpc.ApiException;
 import com.google.cloud.pubsub.v1.Publisher;
+import com.google.common.util.concurrent.MoreExecutors;
 import com.google.protobuf.ByteString;
 import com.google.pubsub.v1.PubsubMessage;
 import com.google.pubsub.v1.TopicName;
 import java.io.IOException;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
+import org.threeten.bp.Duration;
 
 public class PublisherExample {
   public static void main(String... args) throws Exception {
     // TODO(developer): Replace these variables before running the sample.
-    String projectId = "your-project-id";
-    String topicId = "your-topic-id";
+    String projectId = "my-project";
+    String topicId = "my-topic";
 
     publisherExample(projectId, topicId);
   }
@@ -43,17 +51,60 @@ public class PublisherExample {
 
     Publisher publisher = null;
     try {
-      // Create a publisher instance with default settings bound to the topic
-      publisher = Publisher.newBuilder(topicName).build();
+      FlowControlSettings flowControlSettings =
+          FlowControlSettings.newBuilder()
+              // Block more messages from being published when the limit is reached. The other
+              // options are Ignore (or continue publishing) and ThrowException (or error out).
+              .setLimitExceededBehavior(LimitExceededBehavior.Block)
+              .setMaxOutstandingRequestBytes(10 * 1024 * 1024 * 1024L) // 10 GiB
+              .setMaxOutstandingElementCount(300000L) // 300000 messages
+              .build();
 
-      String message = "Hello World!";
-      ByteString data = ByteString.copyFromUtf8(message);
-      PubsubMessage pubsubMessage = PubsubMessage.newBuilder().setData(data).build();
+      // By default, messages are not batched.
+      BatchingSettings batchingSettings =
+          BatchingSettings.newBuilder()
+              .setFlowControlSettings(flowControlSettings)
+              .setElementCountThreshold(1000L)
+              .setRequestByteThreshold(10 * 1024 * 1024L)
+              .setDelayThreshold(Duration.ofMillis(1000))
+              .build();
 
-      // Once published, returns a server-assigned message id (unique within the topic)
-      ApiFuture<String> messageIdFuture = publisher.publish(pubsubMessage);
-      String messageId = messageIdFuture.get();
-      System.out.println("Published message ID: " + messageId);
+      publisher = Publisher.newBuilder(topicName).setBatchingSettings(batchingSettings).build();
+
+      long published = 0;
+      while (true) {
+        String message = "{\"event_time\": 344, \"user_id\": 145065, \"page\": \"page1.html\", \"duration\": 300}";
+        ByteString data = ByteString.copyFromUtf8(message);
+        PubsubMessage pubsubMessage = PubsubMessage.newBuilder().setData(data).build();
+
+        // Once published, returns a server-assigned message id (unique within the topic)
+        ApiFuture<String> future = publisher.publish(pubsubMessage);
+        ApiFutures.addCallback(
+            future,
+            new ApiFutureCallback<String>() {
+
+              @Override
+              public void onFailure(Throwable throwable) {
+                if (throwable instanceof ApiException) {
+                  ApiException apiException = ((ApiException) throwable);
+                  // details on the API exception
+                  System.out.println(apiException.getStatusCode().getCode());
+                  System.out.println(apiException.isRetryable());
+                }
+                System.out.println("Error publishing message : " + message);
+              }
+
+              @Override
+              public void onSuccess(String messageId) {
+                // Once published, returns server-assigned message ids (unique within the topic)
+                // System.out.println("Published message ID: " + messageId);
+              }
+            },
+            MoreExecutors.directExecutor());
+        if (++published % 100000 == 0) {
+          System.out.println("Published " + published);
+        }
+      }
     } finally {
       if (publisher != null) {
         // When finished with the publisher, shutdown to free up resources.
